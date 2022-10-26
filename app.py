90ae7f23ab2781f06cb99b969c90ba3ea3c4b69d
xcraven <sahanherath@hotmail.com>
2022-10-26 15:03:00 +0800

    enable updating of csv

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
+import io
 from datetime import datetime
+import pandas as pd
 from werkzeug.utils import secure_filename
 #from azure.storage.blob import BlockBlobService
 from azure.storage.blob import BlobServiceClient
@@ -24,12 +26,20 @@ def upload_file():
         name_cap = request.form.get('name')
         type_cap = request.form.get('type')
         filename = secure_filename(file.filename)
+        filename_csv = 'upload_list.csv'
         fileextension = filename.rsplit('.',1)[1]
         Randomfilename = id_generator()
         filename = Randomfilename + '.' + fileextension
 
         blob_client = service.get_blob_client(blob = filename, container = 'uploads')
         blob_client.upload_blob(file, blob_type="BlockBlob")
+
+        blob_client_csv = service.get_blob_client(blob = filename_csv, container = 'uploads')
+        blob_data = blob_client_csv.download_blob()
+        dataframe_blobdata = pd.read_csv(io.StringIO(blob_data))
+        dataframe_blobdata = dataframe_blobdata.append({'date_time': datetime.now(), 'filename': filename}, ignore_index=True)
+        stream = io.StringIO()
+        blob_client_csv.upload_blob(dataframe_blobdata.to_csv(stream, sep=","), blob_type="BlockBlob")
 
         return redirect(url_for('hello', name=name_cap, type = type_cap))      
 
